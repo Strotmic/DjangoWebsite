15633cb416b1fa7c53783846d665b7af4898c44a
Tim Bleuzé <tim.bleuze@gmail.com>
2022-03-13 21:47:38 +0100

    Add function works, working on delete

--- ShoppingList/main/urls.py
+++ ShoppingList/main/urls.py
@@ -3,7 +3,9 @@ from . import views
 from main.views import productList
 
 urlpatterns = [
-    path('hello/', views.say_hello),
+    path('hello/', views.tabel),
+    path('add/', views.add_product),
+    path('delete/', views.delete_product),
     path('productlist/', productList.as_view()),
     
 ]
--- ShoppingList/main/views.py
+++ ShoppingList/main/views.py
@@ -1,64 +1,9 @@
-from math import prod
-from django.db import connection
 from django.shortcuts import render
-from django.http import HttpResponse
-from matplotlib.pyplot import connect
-import mysql.connector
-from mysql.connector import Error
-import re
-from numpy import size
-import socket
-from main.models import winkelProduct
-from django.views.generic import ListView
+from matplotlib.style import context
+from main.models import *
+from django.shortcuts import redirect
 
-
-
-mysql.connector.connect(user='root', password='Gusco10.+789',
-                              host='127.0.0.1',
-                              database='shoppinglist')
-con = connection.cursor()
-products= []
-class productList(ListView):
-    model = winkelProduct
-
-def calculate_aantal():
-    products =[]
-    longlist=[]
-    z=con.execute("select * from products")
-    for i in range(int(z)):
-        i+=1
-        longlist.append(i)
-    for id in longlist:
-        con.execute(f"select * from products where id = {id}")
-        con.execute(f"select naam from products where id={id}")
-        naam = con.fetchall()
-        naam = re.sub("[(),]","",str(naam))
-        con.execute(f"select gewicht_een from products where id={id}")
-        gewichteen = con.fetchall()
-        gewichteen = re.sub("[(),]","",str(gewichteen))
-        z = con.execute(f"select gewicht_totaal from products where id={id}")
-        gewichttotaal = con.fetchall()
-        gewichttotaal = re.sub("[(),]","",str(gewichttotaal))
-        if(type(gewichteen)==float):
-            aantal = gewichttotaal/gewichteen
-        else:
-            aantal = float(gewichttotaal)/float(gewichteen)
-        if aantal<2:
-            order = True
-        else:
-            order = False
-        products.append( winkelProduct(id,naam,gewichteen,gewichttotaal,aantal,order))
-    return products
-
-def isnodig():
-    x = calculate_aantal()
-    for obj in x:
-
-        if(obj.aantal<=1):
-            con.execute(f"update products set nodig=true where id={obj.id}")
-
-
-def say_hello(request):
+def tabel(request):
     isnodig()
     x = calculate_aantal()
     z = x
@@ -70,6 +15,22 @@ def say_hello(request):
         kopen = obj.order
     return render(request, 'test.html', {'lijst':x})
 
-    
+def add_product(request):
+    z = con.execute("select * from products")
+    if request.method == 'POST':
+        name = request.POST['name']
+        aantal = request.POST['aantal']
+        weight = request.POST['weight']
+        total_weight = float(weight)*float(aantal)
+        done =False
+        new_product = winkelProduct(z+1,name,weight,aantal,total_weight,done)
+        con.execute(f"insert into products(id,naam,aantal,gewicht_een,gewicht_totaal,nodig) values({z+1},'{name}',{aantal},{weight},{total_weight},{done})")
+        tabel(request)
+        return redirect('/main/hello/')
+        pass
+    return render(request, 'addProduct.html')
 
+def delete_product(request):
+    con.execute(f"DELETE FROM products WHERE id={request.POST['id']}")
+    return redirect('/main/hello/')
 # Create your views here.
